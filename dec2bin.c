738c0943696c66337daa4a30d3d189556fc8b77b
Ayush Agarwal <agarwalayush161@gmail.com>
2022-01-07 10:48:49 -0800

    Starter code

---
+++ dec2bin.c
@@ -0,0 +1,26 @@
+/**
+ * Assignment-1
+ * CSE30 Username: cs30wi22 (fill out your username)
+ * Class: UCSD CSE30-WI22
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[]) {
+
+
+    if (argc != 2) {
+        fprintf(stderr, "%s wrong number of arguments\n", argv[0]);
+        return (EXIT_FAILURE);
+    }
+
+
+    unsigned int population = (unsigned) atoi(argv[1]);
+  
+    // put your code here (use as many lines as you need)
+
+
+    return EXIT_SUCCESS; 
+}
+
